248779966ecd12cc9e06abaaa94d0bffa1179d1f
Vinzent Brömauer <vinz.corno@web.de>
2024-01-11 17:36:57 +0100

    temp fix crash and bad routing engineering

--- wikiserver.py
+++ wikiserver.py
@@ -14,15 +14,17 @@ def sitemap():
     cont = buildSiteMap()
     return render_template("wiki.html", content=cont, path=request.path)
 
-@app.get("/<path:subpath>")
+@app.get("/text/<path:subpath>")
 def read(subpath):
     readme_text = markdown.markdown(getFileContent("./text/" + subpath))
-    return render_template("wiki.html", content=readme_text, path=request.path)
+    return render_template("wiki.html", content=readme_text, path=request.path.replace("/text", "", 1))
 
 def getFileContent(filepath):
     print(f"trying to open file {filepath}")
     if not os.path.exists(filepath):
         return "<p>file not found</p>"
+    if not filepath.endswith(".md"):
+        return "<p>filetype not supported</p>"
     with open(filepath, "r") as input_file:
         text = input_file.read();
     return text;
@@ -33,7 +35,7 @@ def buildSiteMap():
     for root, dirs, files in os.walk("text"):
         for file in files:
             path = root + "\\" +file
-            path = path.replace("\\", "/").replace("text", "", 1)
-            res.append(f"<li><a href='{path}'>{path}</a></li>")
+            path = path.replace("\\", "/")
+            res.append(f"<li><a href='{path}'>{path.replace("text", "", 1)}</a></li>")
     res.append('</ul>')
     return ' '.join(res)
