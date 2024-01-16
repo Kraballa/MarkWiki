c9ac624518ee266da1f587bf8f7fbe19468a377f
Vinzent Brömauer <vinz.corno@web.de>
2024-01-16 16:10:36 +0100

    explain newline extension
    Explain why the newline extension is necessary.

--- addnewline.py
+++ addnewline.py
@@ -1,6 +1,8 @@
 from markdown.preprocessors import Preprocessor
 from markdown.extensions import Extension
 
+# The markdown extension does not allow lists following text without an extra newline.
+# So all lists are prepended with an empty line as a bandaid fix.
 class NewLiner(Preprocessor):
     def run(self, lines):
         new_lines = []
