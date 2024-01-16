a9bca80b46ea3d087c9afb8b9b8a8695cf120713
Vinzent Brömauer <vinz.corno@web.de>
2024-01-16 18:39:04 +0100

    fix some jank regarding lists

--- addnewline.py
+++ addnewline.py
@@ -6,9 +6,9 @@ from markdown.extensions import Extension
 class NewLiner(Preprocessor):
     def run(self, lines):
         new_lines = []
-        for line in lines:
-            if len(line) > 0:
-                if(line.startswith("*") or line.startswith("1.") or line.startswith("-")):
+        for i, line in enumerate(lines):
+            if len(line) > 0 and i > 0:
+                if line.startswith(("*","1.","-")) and not lines[i-1].startswith(("*","1.","-")):
                     new_lines.append("")
             new_lines.append(line)
         return new_lines
